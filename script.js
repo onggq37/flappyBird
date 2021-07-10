57fda137acbf03afc812cf4f13afef68a9445535
onggq37 <84619503+onggq37@users.noreply.github.com>
2021-07-10 15:05:56 +0800

    pipe created

--- script.js
+++ script.js
@@ -20,31 +20,50 @@ pipeSouth.src = "images/pipeSouth.png";
 let bX = 10;
 let bY = 150;
 
-let gravity = 5;
+let gravity = 1;
 //gap between the pipes
-let gap = 85;
-let pX = bg.width
+let gap = 100;
+let pX = cvs.width;
+let pipe = [];
+
+pipe[0] = {
+    x: pX,
+    y: 0
+}
 
 //floor
-let floor = bg.height-fg.height;
+let floor = cvs.height-fg.height;
 
 document.addEventListener("keydown",moveup);
 
 function moveup() {
     bY -= 50;
 }
-//position of pip
+//position of pip 
 function draw() {
     // pipeSouth.onload = function() {}
     ctx.drawImage(bg,0,0);
-    ctx.drawImage(pipeNorth,pX,0);
-    ctx.drawImage(pipeSouth,pX,pipeNorth.height+gap);
+
+    for(let i=0; i<pipe.length;i++) {
+        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
+        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+pipeNorth.height+gap); 
+        
+        pipe[i].x--;
+
+        if(pipe[i].x === 125) {
+            pipe.push ({
+                x: cvs.width,
+                y: Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height,
+             }
+            )
+        }  
+    }
     ctx.drawImage(bird,bX,bY);
     ctx.drawImage(fg,0,floor)
     if (bY <= floor-bird.height) {
         bY += gravity;
     }
-    pX -= 1;
+    
     
     //console.log(bY);
     requestAnimationFrame(draw);
