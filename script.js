81bbdc806d200242e95fbf81b337254afaa1f58d
onggq37 <84619503+onggq37@users.noreply.github.com>
2021-07-22 20:49:37 +0800

    add gravity

--- script.js
+++ script.js
@@ -76,8 +76,12 @@ const bird = {
     y: 150,
     
     frame: 0,
-    period: 0,
+    period: 0, //frequency that the bird is flapping
     
+    gravity: 0.2,
+    speed: 0,
+    jump: 4.5,
+
     draw: function() {
         let bird = this.animation[this.frame];
 
@@ -85,7 +89,7 @@ const bird = {
     },
 
     flap: function() {
-
+        this.speed = -this.jump;
     },
 
     update: function() {
@@ -102,6 +106,19 @@ const bird = {
             this.frame = this.frame%this.animation.length; //ensure that frames go in loop
         }
 
+        if (state.current == state.getReady) {
+            this.y = 150; //reset bird position
+        } else {
+            if (this.y+this.h/2 >= cvs.height-fg.h) {
+                this.y = cvs.height-fg.h-this.h/2;
+                if(state.current == state.game) {
+                    state.current = state.gameOver;
+                }
+            } else {
+                this.speed += this.gravity;
+                this.y += this.speed;  
+            }
+        }
     }
 }
 
@@ -161,73 +178,3 @@ function loop() {
 }
 
 loop();
-/*
-//load images
-const bird = new Image();
-const bg = new Image();
-const fg = new Image();
-const pipeNorth = new Image();
-const pipeSouth = new Image();
-
-bird.src = "images/bird.png";
-bg.src = "images/bg.png";
-fg.src = "images/fg.png";
-pipeNorth.src = "images/pipeNorth.png";
-pipeSouth.src = "images/pipeSouth.png";
-
-//bird intial position
-let bX = 10;
-let bY = 150;
-
-let gravity = 1;
-//gap between the pipes
-let gap = 100;
-let pX = cvs.width;
-let pipeDist = 80;
-let pipe = [];
-
-pipe[0] = {
-    x: pX,
-    y: 0
-}
-
-//floor
-let floor = cvs.height-fg.height;
-
-document.addEventListener("keydown",moveup);
-
-function moveup() {
-    bY -= 50;
-}
-//position of pip 
-function draw() {
-    // pipeSouth.onload = function() {}
-    ctx.drawImage(bg,0,0);
-
-    for(let i=0; i<pipe.length;i++) {
-        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
-        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+pipeNorth.height+gap); 
-        
-        pipe[i].x--;
-
-        if(pipe[i].x === pipeDist) {
-            pipe.push ({
-                x: cvs.width,
-                y: Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height,
-             }
-            )
-        }  
-    }
-    ctx.drawImage(bird,bX,bY);
-    ctx.drawImage(fg,0,floor)
-    if (bY <= floor-bird.height) {
-        bY += gravity;
-    }
-    
-    
-    //console.log(bY);
-    //requestAnimationFrame(draw);
-}
-
-draw();  
-*/
